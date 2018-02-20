c95d6987c3a1c62a64af0e0984fbd61f6134acbe
Stijn Hoeks <slqhoeks@gmail.com>
2018-02-20 09:33:28 +0100

    Basis structuur compleet

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,61 +1,75 @@
 
 
-
-// geld voor alle geladen (ook externe) scripts
-// "use strict"
-
 (function() {
 
-    // geld alleen voor dit script
-    /*"use strict"*/
+    "use strict"
 
     // initialize application
     var app = {
         init: function() {
-
-            collection.init();
-            
+            api.init();
+            routes.init();
+            if ( window.location.hash.substring(1) == '' ) {
+                window.location.href = '#start';
+            }
         }
     }
 
-    var collection = {
+    var api = {
         init: function() {
-            this.get();
+            this.request();
         },
-        get: function() {
-            // dbuOrGB7xoks2WobqPacpFP6fODFIU7gR0rStswa
+        request: function() {
+            // api_key=dbuOrGB7xoks2WobqPacpFP6fODFIU7gR0rStswa
             var self = this;
-
             var request = new XMLHttpRequest();
-
             request.open('GET', 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=dbuOrGB7xoks2WobqPacpFP6fODFIU7gR0rStswa', true);
 
             request.onload = function() {
                 if (request.status >= 200 && request.status < 400) {
-                    var data = JSON.parse(request.responseText);
-                    self.clean(data);
-                } else {
-                    //console.log('Request niet toegestaan')
+                    var rawData = JSON.parse(request.responseText);
+                    collection.init(rawData);
+                } else { //console.log('Request niet toegestaan')
+                    api.error();
                 }
             };
-            request.onerror = function() {
-                //console.log('Geen response van de server')
+            request.onerror = function() { //console.log('Geen response van de server')
+                api.error();
             };
 
             request.send();
-
-            routes.init();
+            
         },
-        clean: function(data) {
+        error: function(rawData) {
 
-            let dataArray = Object.values(data.photos)
-
-            let newArray = dataArray.filter(dataObject => dataObject.rover.status === 'inactive')
-
-            content.init(data)
+            var errorMessage = document.createElement('article');
+            errorMessage.id = 'errorMessage';
+            document.getElementById('start').appendChild(errorMessage);
+            shaven.default(
+                [document.getElementById('errorMessage'),
+                    ['img', {src:'static/img/404.jpg'} ],
+                    ['article',
+                        ['h1', 'SERVER ERROR'],
+                        ['p', 'De request kon niet worden voltooid'],
+                        ['a', 'Probeer opnieuw', {href:'index.html'} ]
+                    ]
+                ]
+            )
         }
     }
 
+    var collection = {
+        init: function(rawData) {
+            this.clean(rawData);
+        },
+        clean: function(rawData) {
+            //let dataArray = Object.values(rawData.photos)
+            //let data = dataArray.filter(dataObject => dataObject.rover.status === 'inactive')
+            var data = rawData;
+            content.init(data);
+
+        }
+    }
 
     // handle routes & states
     var routes = {
@@ -63,43 +77,24 @@
             this.handleEvent();
         },
         handleEvent: function() {
-
+            var self = this;
             routie({
-                '': function() {
-                },
-                'start': function() {
-                    var $this = this.path;
-                    routes.current($this);
-                },
-                'bestpractices': function() {
-                    var $this = this.path;
-                    routes.current($this);
-                },
-                'modular': function() {
-                    var $this = this.path;
-                    routes.current($this);
-                },
-                'modularder': function() {
-                    var $this = this.path;
-                    routes.current($this);
-                },
                 ':id': function() {
                     var $this = this.params.id;
-                    routes.current($this);
+                    self.current($this);
                 }
             });
-
         },
         current: function($this) {
-
 
             var routes = document.querySelectorAll('section.active')
             for (var i = 0; i < routes.length; i++) {
                 routes[i].classList.remove('active');
             }
-
             var targetElement = document.getElementById($this);
-            targetElement.classList.add('active');
+            if ( targetElement ) {
+                targetElement.classList.add('active');
+            }
 
         }
 
@@ -115,36 +110,25 @@
         create: function(data) {
             
             for (var i = 0; i < data.photos.length; i++) {
-
                 var newEntry = document.createElement('a');
-                newEntry.id = 'main_'+data.photos[i].id;
-                newEntry.href = '#detail_'+data.photos[i].id;
+                newEntry.id = 'main_'+i;//data.photos[i].id;
+                newEntry.href = '#detail';
                 document.getElementById('start').appendChild(newEntry);
-
-                var newEntry_inner = document.createElement('section');
-                newEntry_inner.id = 'detail_'+data.photos[i].id;
-                document.getElementById('body').appendChild(newEntry_inner);
-
             }
-
             this.render(data)
 
         },
         render: function(data) {
 
-
             for (var i = 0; i < data.photos.length; i++) {
-            
                 shaven.default(
-                    [document.getElementById('main_'+data.photos[i].id),
+                    [document.getElementById('main_'+i),//data.photos[i].id
                         ['div', {
-                            style: {
-                                'background-image': 'url('+data.photos[i].img_src+')',
-                            },
+                            style: { 'background-image': 'url('+data.photos[i].img_src+')' },
                         }],
                         ['ul',    
                             ['li', 'Camera',
-                                ['p',data.photos[i].camera.full_name]
+                                ['p',data.photos[i].camera.name]
                             ],
                             ['li', 'ID',
                                 ['p',data.photos[i].id]
@@ -153,39 +137,70 @@
                     ]
                 )
             }
-            this.detail(data)
-        },
-        detail: function(data) {
-
-            for (var i = 0; i < data.photos.length; i++) {
-            
-                shaven.default(
-                    [document.getElementById('detail_'+data.photos[i].id),
-                        ['div', {
-                            style: {
-                                'background-image': 'url('+data.photos[i].img_src+')',
-                            },
-                        }],
-                        ['ul',    
-                            ['li', 'Camera',
-                                ['p',data.photos[i].camera.full_name]
-                            ],
-                            ['li', 'ID',
-                                ['p',data.photos[i].id]
-                            ],    
-                        ]   
-                    ]
-                )
-            }
-            
+            detail.init(data)
         }
-
-
-
     }
 
+    var detail = {
+        init: function(data) {
 
+            this.create(data)
 
+        },
+        create: function(data) {
+
+            var newEntry_detail = document.createElement('section');
+            newEntry_detail.id = 'detail';
+            document.getElementById('body').appendChild(newEntry_detail);
+
+            this.render(data);
+            
+        },
+        render: function(data) {
+
+            shaven.default(
+                [document.getElementById('detail'),
+                    ['header',
+                        ['h1',
+                            ['a','home', {href:'index.html#start'}]],
+                        ['p','>'],
+                        ['p', 'detail' ]
+                    ],
+                    ['img', {id: 'detail_img'} ],
+                    ['ul',    
+                        ['li', 'ID',
+                            ['p', {id: 'detail_id'} ]
+                        ],
+                        ['li', 'CAM',
+                            ['p', {id: 'detail_cam'} ]
+                        ],    
+                    ]   
+                ]
+            )
+
+            this.select(data);
+
+        },
+        select: function(data) {
+            var self = this;
+            var allElements = document.getElementById('start').getElementsByTagName('a');
+            for (var i = 0; i < allElements.length; i++) {
+                allElements[i].addEventListener("click", function() {
+                    var aCurrent = this;
+                    var aIndex = aCurrent.id.substr(-1);
+                    console.log(aIndex)
+                    self.update(data,aIndex);
+                });    
+            }
+        },
+        update: function(data,aIndex) {
+
+            document.getElementById('detail_img').src       = data.photos[aIndex].img_src;
+            document.getElementById('detail_id').innerHTML  = data.photos[aIndex].id;
+            document.getElementById('detail_cam').innerHTML = data.photos[aIndex].camera.name;
+
+        }
+    }
 
     // roep de functie app.init aan
     app.init()
